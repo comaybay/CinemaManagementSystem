b49beb9e9ec83f765744892d4227ec5a048a6599
Thái Chí Bảo <comaybayy@gmail.com>
2022-05-24 23:34:15 +0700

    thêm thông báo nếu tài khoản là admin

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -30,13 +30,16 @@ export default function Header() {
             </ul>
           </Link>
           :
-          <ul className="flex space-x-2">
+          <ul className="flex space-x-2 items-center">
             <li > <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
               <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
             </svg></li>
             <li><p href="#" className="text-3xl" >
               {profile.name}
             </p></li>
+            {
+              profile.is_admin && <p className="text-red-700 text-xl">(Admin)</p>
+            }
             <button className="ml-4 text-3xl hover:text-blue-700" onClick={signOut}>đăng xuất</button>
           </ul>
         }
